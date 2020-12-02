98e880fc7b3b88bc1a323657df3033ff84736152
josuekongolo <44091148+josuekongolo@users.noreply.github.com>
2020-12-02 08:36:48 +0100

    Add files via upload

---
+++ Storytime/About us/About_us.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>About us</title>
+	<link rel="stylesheet" a href="stylesheet/main.css">
+	<link rel="icon" a href="img/snapfood.png">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+</body>
+</html>
---
+++ Storytime/Stories/Stories.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Stories</title>
+	<link rel="stylesheet" a href="stylesheet/main.css">
+	<link rel="icon" a href="img/snapfood.png">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+</body>
+</html>
