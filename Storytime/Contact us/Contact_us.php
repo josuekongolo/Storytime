832d5902ffffc0f5f7981c2e084870aba69bf5a3
josuekongolo <44091148+josuekongolo@users.noreply.github.com>
2020-12-02 09:50:08 +0100

    Add files via upload

--- Storytime/Contact us/Contact_us.php
+++ Storytime/Contact us/Contact_us.php
@@ -3,11 +3,16 @@
 <head>
 <meta charset="utf-8">
 <title>Contact us</title>
-	<link rel="stylesheet" a href="stylesheet/main.css">
+	<link rel="stylesheet" a href="../stylesheet/main.css">
 	<link rel="icon" a href="">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Lobster&family=Roboto:wght@300&display=swap" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
+	<?php include '../nav.php';?>
 </body>
 </html>
---
+++ Storytime/nav.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Navbar</title>
+<link rel="stylesheet" href="Stylesheet/main.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Lobster&family=Roboto:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div id="navbar" class="navbar">
+		<div class="navbar_links">
+			<ul class="navbar1">
+				<h1 style="">Storytime</h1>
+				<li><a href="../Home/Home.php">Home</a></li>
+				<!--<li><a href="../om-oss/om-oss.php" >Om oss</a></li>-->
+				<li><a href="../Stories/Stories.php">Stories</a></li>
+				<li><a href="../About us/About_us.php">About us</a></li>
+				<li><a href="../Contact us/Contact_us.php">Contact us</a></li>
+			</ul>
+		</div>
+	</div>
+</body>
+</html>
