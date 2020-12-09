a9f278ab7b81461f366833d30b0b3b9cdcf43efb
_ItZDickaProYT_ <51058801+Sanderjoh@users.noreply.github.com>
2020-12-09 09:23:56 +0100

    Add files via upload

--- Storytime/Home/Home.php
+++ Storytime/Home/Home.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title>Home</title>
-	<link rel="stylesheet" a href="../stylesheet/main.css">
+	<link rel="stylesheet" a href="../Stylesheet/main.css">
 	<link rel="icon" a href="img/snapfood.png">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
@@ -14,5 +14,50 @@
 
 <body>
 	<?php include '../nav.php';?>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<h3 class="nettsider">Underoverskrift som skal si noe om de tre eksemplene under</h3><br>
+<div class="body">
+	<table class="content">
+	     <tr>
+	       <td>
+	         <div>
+	           <a href="#" target="_blank"><br>
+	             <img alt="Familie #1" src="Bilder/"></a><br><br>
+	             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+								 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+	         </div>
+	       </td>
+	       <td>
+	         <div>
+	           <a href="#" target="_blank"><br>
+	             <img alt="Familie #2" src="Bilder/"></a><br><br>
+	             <p>
+	             	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+								Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+								Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	             </p>
+
+	        </div>
+	      </td>
+	      <td class="Third">
+	        <div>
+	          <a href="#" target="_blank"><br>
+	            <img alt="Familie #3" src="Bilder/"></a><br><br>
+	            <p>
+	            	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+								Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+								Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	            </p>
+
+	        </div>
+	     </td>
+	   </tr>
+	</table>
+</div>
+
 </body>
 </html>
